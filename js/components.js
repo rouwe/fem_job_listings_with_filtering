5efa3dba49828f747d6eb092ddf3d4162771813a
rouwe <santosroweme14@gmail.com>
2022-02-02 10:06:39 +0800

    Update class namespace and use jQuery

--- js/components.js
+++ js/components.js
@@ -1,24 +1,38 @@
 // Renders components from JSON data
-let dataObject = [];
-fetch('../data.json')
-// Get data
-    .then(data => data.json())
-    .then(data => {
-        for(const obj of data) {
-            dataObject.push(obj);
+$(document).ready(() => {
+    class ElementBuilder {
+        /* Class for building elements
+           :param buildName: String - element name
+           :param buildClass: String - class to add in element
+           :param buildCount: Number - number of elements 
+        */
+        constructor(buildName, buildClass, buildCount) {
+            this.buildName = buildName;
+            this.buildClass = buildClass;
+            this.buildCount = buildCount;
         }
-    })
-    .catch(data => {
-        dataObject = {};
-        console.log(error);
+        generateContainer() {
+            // Generate containers for each available job
+            const parentElement = document.getElementById("main-container");
+            const container = document.createElement('div');
+            parentElement.appendChild(container);
+            container.classList.add()
+            console.log(parentElement, container)
+        }
+    }
+    const test = new ElementBuilder('div', 'job-container', 1);
+    console.log(test.constructor)
+    // Fetch JSON file
+    $.getJSON("../data.json", (result, status) => {
+        if (status === "success") {
+            // for (const key in result[0]) {
+            //     if (key === 'languages' || key === 'tools') {
+            //         console.log(result[0][key])
+            //     }
+            // };
+            console.log(result[0])
+        } else {
+            console.log(`Error:${status}`);
+        }
     });
-
-function generateContainer() {
-    // Generate containers for each available job
-    const parentElement = document.getElementById("main-container");
-
-    const container = document.createElement('div');
-    parentElement.appendChild(container);
-    console.log(parentElement, container)
-}
-// generateContainer()
+})
