d33e58b96a3d66d084f8cc2725a0498d5a5620c2
rouwe <santosroweme14@gmail.com>
2022-02-10 08:46:30 +0800

    Filter by languages and tools update

--- js/filter.js
+++ js/filter.js
@@ -18,7 +18,57 @@ function filterByLevel() {
 }
 function filterByLanguages() {
     // Filter job lists dependng on languages    
+    const languageTagText = this.innerHTML;
+    const jobContainers = $('.tag-box');
+    const toDisplaySet = new Set();
+    for (const job of jobContainers) {
+        let counter = 2;
+        while (counter < $(job)[0].childNodes.length) {
+            const firstClass = $(job)[0].childNodes[counter].classList[0];
+            const elementText = $(job)[0].childNodes[counter].innerHTML;
+            if (firstClass !== 'languages') {
+                break;
+            }
+            $(job).parent().css("display", "none");
+            if (elementText === languageTagText) {
+                toDisplaySet.add($(job).parent());
+            }
+            counter = counter + 1;
+        }
+    }
+    for (const toDisplay of toDisplaySet) {
+        $(toDisplay[0]).css("display", "flex");
+    }
 }
 function filterByTools() {
     // Filter job lists depending on tools
+    const toolsTagText = this.innerHTML;
+    const jobContainers = $('.tag-box');
+    const toDisplaySet = new Set();
+    const toCloseSet = new Set();
+    for (const job of jobContainers) {
+        let counter = 2;
+        while (counter < $(job)[0].childNodes.length) {
+            const firstClass = $(job)[0].childNodes[counter].classList[0];
+            const elementText = $(job)[0].childNodes[counter].innerHTML;
+            if (firstClass !== 'tools') {
+                counter = counter + 1;
+                toCloseSet.add($(job).parent())
+                continue;
+            }
+            $(job).parent().css("display", "none");
+            if (elementText === toolsTagText) {
+                toDisplaySet.add($(job).parent());
+            }
+            counter = counter + 1;
+        }
+    }
+    // Hide all elements in toCloseSet
+    for (const toClose of toCloseSet) {
+        $(toClose[0]).css("display", "none");
+    }
+    // Display all elements in toDisplaySet
+    for (const toDisplay of toDisplaySet) {
+        $(toDisplay[0]).css("display", "flex");
+    }
 }
